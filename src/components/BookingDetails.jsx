a79383b611f8c83225e88f50009efcc4d46dd071
yogzsp <yogiyogi.prana43@gmail.com>
2024-11-26 23:19:54 +0800

    rapiin modal

--- src/components/BookingDetails.jsx
+++ src/components/BookingDetails.jsx
@@ -6,24 +6,42 @@ const BookingDetails = ({ details, onClose }) => {
 
   return (
     <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center z-50">
-      <div className="bg-black p-6 rounded-lg w-2/3 md:w-1/3">
-        <h2 className="text-xl font-bold mb-4">Rincian Pemesanan</h2>
-        <p><strong>Nama Pengendara:</strong> {details.username}</p>
-        <p><strong>Plat Nomor:</strong> {details.vehicle}</p>
-        <p><strong>Tempat Parkir:</strong> {details.slot}</p>
-        <p><strong>Waktu Mulai:</strong> {formattedStartTime}</p>
-        <p><strong>Durasi:</strong> {details.duration} jam</p>
-
-        <div className="mt-4">
-          <button
-            onClick={onClose} // Fungsi ini hanya menutup modal
-            className="bg-gray-500 text-white p-2 rounded"
-          >
-            Tutup
-          </button>
-        </div>
+  <div className="bg-black p-6 rounded-lg w-3/4 md:w-1/3 text-white">
+    <h2 className="text-xl font-bold mb-4">Rincian Pemesanan</h2>
+    <div className="space-y-2">
+      <div className="grid grid-cols-2">
+        <strong>Nama Pengendara</strong>
+        <span>: {details.username}</span>
+      </div>
+      <div className="grid grid-cols-2">
+        <strong>Plat Nomor</strong>
+        <span>: {details.vehicle}</span>
+      </div>
+      <div className="grid grid-cols-2">
+        <strong>Tempat Parkir</strong>
+        <span>: {details.slot}</span>
+      </div>
+      <div className="grid grid-cols-2">
+        <strong>Waktu Mulai</strong>
+        <span>: {formattedStartTime}</span>
+      </div>
+      <div className="grid grid-cols-2">
+        <strong>Durasi</strong>
+        <span>: {details.duration} jam</span>
       </div>
     </div>
+
+    <div className="mt-4 text-center">
+      <button
+        onClick={onClose} // Fungsi ini hanya menutup modal
+        className="bg-gray-500 text-white px-4 py-2 rounded"
+      >
+        Tutup
+      </button>
+    </div>
+  </div>
+</div>
+
   );
 };
 
